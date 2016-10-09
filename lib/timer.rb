9b7edf962dc72fae6b040283901f6bef0b45c9f8
Robert Smith <robert.smith160@icloud.com>
2016-10-09 17:50:58 -0600

    "Finished timer file."

--- lib/timer.rb
+++ lib/timer.rb
@@ -1,36 +1,38 @@
 class Timer
-  attr_accessor :end, :start_time
+  attr_accessor :start_game, :end_game, :time
   attr_reader :elapsed_time
 
     def initialize
       @time = Time.new
       @elapsed_time = nil
-      @start_time = start_time
-      @end_time = time_to_seconds
+      @start_game = false
+      @end_game = nil
     end
 
-    def start_time_convert
-      @start_time = time_to_seconds
+    def time_convert
+      if @end_game.eql?(true)
+        @end_time = time_to_seconds
+        time_difference_calculation
+      elsif @start_game.eql?(true)
+        @start_time = time_to_seconds
+      end
     end
 
     def time_to_seconds
       (@time.min * 60) + @time.sec
     end
 
-    def end_of_game
-      time_difference_calculation(end_time,start_time)
-    end
-
-    def time_difference_calculation(end_time,start_time)
-      seconds_formatter(end_time - start_time)
+    def time_difference_calculation
+      seconds = @end_time - @start_time
+      seconds_formatter(seconds)
     end
 
     def seconds_formatter(seconds)
       if seconds < 3600
-        @elapsed_time = Time.at(seconds).strftime("%M:%S")
+        @elapsed_time = Time.at(seconds).strftime("%M minutes and %S seconds")
       else
         hour = seconds / 3600
-        @elapsed_time = "#{hour}:#{Time.at(seconds).strftime("%M:%S")}"
+        @elapsed_time = Time.at(seconds).strftime("#{hour} hours and %M minutes and %S seconds")
       end
     end
 
