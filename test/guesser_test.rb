600223ecbd72d68fdd1077386a2178382790abd4
Robert Smith <robert.smith160@icloud.com>
2016-10-09 17:48:20 -0600

    "Finished guesser_test file"

--- test/guesser_test.rb
+++ test/guesser_test.rb
@@ -4,6 +4,7 @@ require './lib/guesser.rb'
 class TestGuesser < Minitest::Test
   def setup
     @guess = Guesser.new
+    @guess.user_input = 'bgry'
     @guess.code = 'bgry'
   end
 
@@ -13,41 +14,46 @@ class TestGuesser < Minitest::Test
   end
 
   def test_checks_input_only_includes_alpha_characters
-    user_input = 'bg76*'
-    assert_equal "No special characters or numbers.", @guess.check_input_for_validity(user_input)
+    @guess.user_input = 'bg76*'
+    expected = "No special characters or numbers."
+    assert_equal expected, @guess.check_input_for_validity
   end
 
   def test_returns_incorrect_input_length
-    user_input ='bgr'
-    refute_equal @guess.code.length, user_input.length
-    assert_equal "Your input is to short.", @guess.check_input_for_validity(user_input)
-    user_input = "bgryk"
-    assert_equal "Your input is to long.", @guess.check_input_for_validity(user_input)
+    @guess.user_input ='bgr'
+    refute_equal @guess.code.length, @guess.user_input.length
+    assert_equal "Your input is to short.", @guess.check_input_for_validity
+    @guess.user_input = "bgryk"
+    assert_equal "Your input is to long.", @guess.check_input_for_validity
   end
 
   def test_compares_user_input_with_code
     # skip
-    @guess.code = 'bgry'
-    @guess.check_input_against_code('bgry')
+    @guess.user_input = "bgry"
+    @guess.check_input_against_code
     assert @guess.correct_code?
   end
 
   def test_returns_number_of_elements_correct_if_guess_is_wrong
     # skip
-    @guess.check_input_against_code('brry')
+    @guess.user_input = 'brry'
+    @guess.check_input_against_code
     refute @guess.correct_code?
+    @guess.user_input = "brrg"
     assert_equal 3, @guess.correct_elements
-    @guess.check_input_against_code('brrg')
+    @guess.check_input_against_code
     refute @guess.correct_code?
     assert_equal 3, @guess.correct_elements
   end
 
   def test_returns_number_of_elements_in_correct_position
     # skip
-    @guess.check_input_against_code('brry')
+    @guess.user_input = "brry"
+    @guess.check_input_against_code
     refute @guess.correct_code?
     assert_equal 3, @guess.correct_position
-    @guess.check_input_against_code('brrg')
+    @guess.user_input = "brrg"
+    @guess.check_input_against_code
     refute @guess.correct_code?
     assert_equal 2, @guess.correct_position
   end
